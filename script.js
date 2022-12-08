92da75caf85cee45e9bbfa49161bb690692e711e
Triple-Edged <usan.siri.trzm@gmail.com>
2022-12-08 08:17:00 -0500

    basic bracket layout

---
+++ script.js
@@ -0,0 +1,20 @@
+// var tree = {
+//     'name': 'Finals',
+//     'children': [
+
+//     ]
+// }
+
+// const tournament_bracket_element = document.createElement("div");
+
+// tournament_bracket_element.className = "tournament_bracket";
+
+// tournament_bracket_element.append(document.createElement("ul"));
+
+// const ul = document.createElement("li");
+// ul.innerText = "Finals";
+// tournament_bracket_element.firstElementChild.append(ul);
+
+
+
+// document.body.append(tournament_bracket_element);
