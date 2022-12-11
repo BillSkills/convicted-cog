a16fe8c7d1e5fa64a424769e58c97af99a73a0cb
Triple-Edged <usan.siri.trzm@gmail.com>
2022-12-10 22:17:07 -0500

    tournament tree is now procedurally generated from an object
    i did it lets goooo
    this took me too long

---
+++ src/components/Match.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: "Match",
+    props: {
+        fight: Object,
+    }
+}
+</script>
+
+<template>
+    <li>
+        <span>{{fight.name}}</span>
+        <ul>
+            <Match 
+            v-for="thisFight in fight.children"
+            :key="thisFight.name"
+            :fight="thisFight"/>
+        </ul>
+    </li>
+</template>
